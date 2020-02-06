3c692d6887cbaa4efb2d25a1f6446c8d8b739adf
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-06 20:05:45 +0100

    Library

---
+++ week2/objectOriented/Book.py
@@ -0,0 +1,26 @@
+class Book():
+    """A simple book model consisting of chapters, which in 
+    turn consist of paragraphs."""
+
+    def __init__(self, title, author, chapters=[]):
+        """Initialize title, the author, and the chapters."""
+        self.title = title
+        self.author = author
+        self.chapters = chapters
+
+    def __repr__(self):
+        return 'Book(%r, %r, %r)' % (self.title, self.author, self.chapters)
+
+    def __str__(self):
+        return '{name} by {by} has {nr_chap} chapters.'.format(
+            name=self.title, by=self.author, nr_chap=len(self.chapters))
+
+    def read(self, chapter=1):
+        """Simulate reading a chapter, by calling the reading 
+        method of a chapter."""
+        self.chapters[chapter - 1].read()
+
+    def open_book(self, chapter=1) -> Chapter:
+        """Simulate opening a book, which returns a chapter 
+        object."""
+        return self.chapters[chapter - 1]
---
+++ week2/objectOriented/Chapter.py
@@ -0,0 +1,18 @@
+class Chapter():
+
+    def __init__(self, number, title, paragraphs):
+        """A chapter consists of multiple paragraphs."""
+        self.number = number
+        self.title = title
+        self.paragraphs = []
+        for paragraph_lines in paragraphs:
+            new_pragraph = Paragraph(paragraph_lines)
+            self.paragraphs.append(new_pragraph)
+
+    def read(self, paragraph_idx=None):
+        """A paragraph can be read.""" 
+        if paragraph_idx:
+            self.paragraphs[paragraph_idx].read()
+        else:
+            for paragraph in self.paragraphs:
+                paragraph.read()
