61bc47cdfe55bc85135ad81f085e45d121f23e66
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-07 22:32:38 +0100

    Update myArgparse.py

--- week2/CLI/myArgparse.py
+++ week2/CLI/myArgparse.py
@@ -3,4 +3,15 @@ import argparse
 parser = argparse.ArgumentParser(
     description='A program that downloads a URL and stores it locally')
 
+parser.add_argument('-u', '--url', help='url of the file you wish to download')
+parser.add_argument('-d', '--destination', default='default_file.exe',
+                    help='The name of the file to store the contents found at the url in')
+
+
+args = parser.parse_args()
+
+myUrl = args.url
+
+print(args.url)
+
 print(parser.parse_args())
