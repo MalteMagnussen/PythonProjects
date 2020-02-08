73b3aa5b808f122d1d538363e8d6f19b825866c7
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-08 11:09:12 +0100

    how importing works

---
+++ importexample/import_demo/README.md
@@ -0,0 +1,4 @@
+For at lave module import på tværs af sibling eller parent folders / packages se dette lille eksempel:
+https://github.com/HartmannDemoCode/import_demo efter git clone, gå ind i repoet og kør koden fra cli med: python -m import_demo.sub1.mod1
+For mere bagrundsinfo læs:
+https://needone.app/python-importing-explained/ (edited)
--- week2/CLI/myArgparse.py
+++ week2/CLI/myArgparse.py
@@ -3,7 +3,10 @@ import argparse
 parser = argparse.ArgumentParser(
     description='A program that downloads a URL and stores it locally')
 
-parser.add_argument('-u', '--url', help='url of the file you wish to download')
+# Positional arg
+parser.add_argument('url', help='url of the file you wish to download')
+
+# Optional Arg [- , --]
 parser.add_argument('-d', '--destination', default='default_file.exe',
                     help='The name of the file to store the contents found at the url in')
 
