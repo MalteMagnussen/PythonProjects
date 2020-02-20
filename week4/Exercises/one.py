1b5e4145dfa26b42ae463a934769426225e7f4fa
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-20 15:18:12 +0100

    Update one.py

--- week4/Exercises/one.py
+++ week4/Exercises/one.py
@@ -1,2 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Turn the csv file into a numpy ndarray with np.genfromtxt(filename, delimiter=',', dtype=np.uint, skip_header=1)
+filename = './befkbhalderstatkode.csv'
+
+data = np.genfromtxt(
+    filename, delimiter=',', dtype=np.uint, skip_header=1)
+
+# Using this data:
+neighb = {1: 'Indre By', 2: 'Østerbro', 3: 'Nørrebro', 4: 'Vesterbro/Kgs. Enghave',
+          5: 'Valby', 6: 'Vanløse', 7: 'Brønshøj-Husum', 8: 'Bispebjerg', 9: 'Amager Øst',
+          10: 'Amager Vest', 99: 'Udenfor'}
+# Find out how many people lived in each of the 11 areas in 2015
