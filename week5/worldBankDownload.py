13559a1fd1aaaaf65ef5e5e295fe4e00b5d14c43
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-25 21:38:16 +0100

    worldbank

--- week5/worldBankDownload.py
+++ week5/worldBankDownload.py
@@ -35,10 +35,20 @@ try:
     data = pd.read_csv(
         'API_EN.ATM.CO2E.KT_DS2_en_csv_v2_800760.csv', skiprows=4)
     columns_names = data.columns
-    print('column names:\n', list(columns_names), '\n\n')
+    # print('column names:\n', list(columns_names), '\n\n')
     countries = data['Country Name']
-    print("{} countries are in the dataset.".format(len(countries)))
-    print('countries are of data type: ', type(countries))
-    print(list(countries))
+    # print("{} countries are in the dataset.".format(len(countries)))
+    # print('countries are of data type: ', type(countries))
+    # print(list(countries))
+    # Create a new pandas Series with countries as labels and a random number as values
+    import random
+    # random.sample takes a population and a sample size k and returns k random members of the population.
+    random_val_pr_country = pd.Series(random.sample(
+        range(1, len(countries)+1), len(countries)), index=countries)
+    print(random_val_pr_country.sort_values())
 except:
     print("File not found")
+
+# instansiate Series from dict
+# new_series = pd.Series(dict({'a':3,'b':6,'c':9}))
+# print(new_series)
