815ad7c4614bdfed9e1b0b3db3c6d005e4ae0254
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-01 15:29:09 +0100

    Done with A

--- week5/exercises/ugifte.py
+++ week5/exercises/ugifte.py
@@ -2,6 +2,7 @@ import os.path
 import pandas as pd
 import requests
 import zipfile
+import matplotlib.pyplot as plt
 
 filename = 'FOLK1A.csv'
 
@@ -27,17 +28,45 @@ else:
     print('-----------------')
     print('Downloaded {}'.format(fname))
 
-    # extract content of zip file in current folder
-    # zipfile.ZipFile(fname, 'r').extractall('.')
-
 
 try:
     # Read CSV
     data = pd.read_csv(
-        filename, skiprows=1, sep=";")
+        filename, sep=";")
     print("__________________\nInside Try Statement:\n")
     print("Type:", type(data))
     print("Columns:", data.columns)
-    print("Iloc[0]:", data.iloc[0, :])
+    print("Iloc[0]:", data.iloc[3, :])
+    fraskilt = data.loc[data.CIVILSTAND ==
+                        "Fraskilt"]["INDHOLD"].values.tolist()
+    print("Fraskilte\n", fraskilt)
+    total = data.loc[data.CIVILSTAND == "I alt"]["INDHOLD"].values.tolist()
+    print("I alt\n", total)
+    tid = data.loc[data.CIVILSTAND == "I alt"]["TID"].values.tolist()
+    print("\nTid:\n", tid)
+    # samlet = pd.merge(fraskilt, total, how="inner", on="TID",
+    #                   suffixes=('_fraskilt', '_total'))
+    # print("samlet:\n", samlet)
+    percentages = [skilt/tot*100 for skilt, tot in zip(fraskilt, total)]
+    print("percentages\n", percentages)
+
+    # Time to plot
+    plt.figure()
+
+    x = tid
+    y = percentages
+    plt.plot(x, y, linewidth=5)
+
+    # Set chart title and label axes.
+    plt.title("Skilte over tid i Danmark", fontsize=24)
+    plt.xlabel("Tid", fontsize=14)
+    plt.ylabel("% af totalbefolkningen er der skilte", fontsize=14)
+    # Set size of tick labels.
+
+    plt.tick_params(axis='both', labelsize=14)
+
+    plt.savefig('skilte.png', bbox_inches='tight')
+    plt.show()
+
 except:
     print("File not found")
