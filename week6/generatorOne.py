032924949e2352a2c224b3c9b91944f7fe85789d
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-02 10:40:49 +0100

    my first generator

--- week6/generatorOne.py
+++ week6/generatorOne.py
@@ -6,15 +6,26 @@ import pandas as pd
 
 df = pd.read_excel(
     r'Alle navne, der er godkendt som både drenge- og pigenavn per 2020-03-02.xls')
-print(df)
+# df = pd.read_excel(
+#     "https://ast.dk/_namesdb/export/names?format=xls&gendermask=4")
+# print(df)
 
 
 def firstn(n):
-    """Our first generator that lazy loads each requested element (as opposed to iterator, that builds the whole list in memory)"""
     num = 0
-    while num < n:
-        yield num
+    while num < len(n):
+        yield n[num]
         num += 1
 
 
-[x for x in firstn(10)]
+#Generator = g
+g = firstn(df["Names"].values.tolist())
+print(next(g))
+
+
+print("\n____DF GENERATOR_____\n")
+
+generator = df.iterrows()
+for idx, row in generator:
+    if idx < 4:
+        print(row["Names"])
