2438d44bc955b1453ff6d8ef6ede2df483fbec61
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-02 19:14:52 +0100

    whats going on man

--- week6/exercises/myModule.py
+++ week6/exercises/myModule.py
@@ -12,7 +12,7 @@
 import requests
 #import NotFoundException
 import threading
-from concurrent.futures import ThreadPoolExecutor
+import concurrent
 import multiprocessing
 import os
 from urllib.parse import urlparse
@@ -31,6 +31,8 @@ class download():
         if filename is None:
             result = urlparse(url)
             filename = os.path.basename(result.path)
+            if filename not in self._url_list:
+                self._url_list.append(filename)
 
         try:
             response = requests.get(url)
@@ -52,6 +54,18 @@ class download():
     def multi_download(self, url_list: list(string)):
         # multi_download(url_list) uses threads to download multiple urls as text and stores filenames as a property
         self._url_list = url_list
-        with ThreadPoolExecutor(len(url_list)) as executor:
-            future = executor.map(download, url_list)
-            print(future.result())
+
+        # We can use a with statement to ensure threads are cleaned up promptly
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(url_list)) as executor:
+            # Start the load operations and mark each future with its URL
+            future_to_url = {executor.submit(
+                download, url): url for url in url_list}
+            for future in concurrent.futures.as_completed(future_to_url):
+                url = future_to_url[future]
+                try:
+                    data = future.result()
+                    # print(data)
+                except Exception as exc:
+                    print('%r generated an exception: %s' % (url, exc))
+                else:
+                    print('%r page is %d bytes' % (url, len(data)))
