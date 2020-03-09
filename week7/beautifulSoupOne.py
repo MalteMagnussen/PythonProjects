3c1c48ec74812cf0ef7a1fc49a79841ecf1071da
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-09 16:15:26 +0100

    Python regex and soup

---
+++ week7/beautifulSoupOne.py
@@ -0,0 +1,35 @@
+import bs4
+import requests
+from tqdm import tqdm
+
+
+def getPages():
+
+    url = "https://www.dr.dk/radio/programmer?side=1"
+    r = requests.get(url)
+    r.raise_for_status()
+
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    pages = soup.findAll("button", {"class": "pagination__item"})
+
+    numberOfPages = pages[-2].getText()
+
+    print(numberOfPages)
+
+    return int(numberOfPages)
+
+
+def getTitles(soup):
+    titles = soup.findAll("h3", {"class": "spot-content__title"})
+
+    print([text.getText() for text in titles])
+
+
+for page in tqdm(range(1, getPages()+1)):
+    # print("PAGE:{}".format(page))
+    url = "https://www.dr.dk/radio/programmer?side={}".format(page)
+    r = requests.get(url)
+    r.raise_for_status()
+
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    getTitles(soup)
