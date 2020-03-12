47a84681cca92fa29ce89c1b372a46727c47979a
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-12 12:22:29 +0100

    Starting Python Exercise

---
+++ week7/charmingReaction/one.py
@@ -0,0 +1,11 @@
+#Charming Reaction	
+# Link	https://www.dba.dk/biler/biler/
+# "1. Hvor mange brugte biler er der at vælge i mellem"	
+# "2. Udskriv alle biler af mærket Ford"	
+# "3. Åben de 5 dyreste biler med selenium 
+# i decending order og vis dem med et bar chart"
+
+import bs4
+import requests
+from tqdm import tqdm
+
