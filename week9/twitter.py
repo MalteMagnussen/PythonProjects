66684ac71f4694cde7fa1e85a893e5747e1855a9
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-23 11:47:07 +0100

    Update twitter.py

--- week9/twitter.py
+++ week9/twitter.py
@@ -13,6 +13,18 @@ print(nx.info(graph))
 
 print("214328887" in graph)
 
+# g.degree giver antallet af edges som støder op mod noden.
+deg_vec = np.array([graph.degree(n) for n in graph.nodes()])
+# deg_vac.max() giver mig bare det højeste tal af numpy arrayet
+max_ind_deg = deg_vec.max()
+# np.argmax() giver indexet af det højeste tal af numpy arrayet
+index_name = list(graph.nodes())[np.argmax(deg_vec)]
+print(
+    "The node {} has the most connections with {} connections".format(
+        index_name, len(list(graph.neighbors(index_name)))
+    )
+)
+
 # in_deg_vec = np.array([graph.in_degree(n) for n in graph.nodes()])
 # max_ind_deg = in_deg_vec.max()
 
@@ -28,6 +40,6 @@ print("214328887" in graph)
 #     page_rank = sub_term  + (d * pass)
 #     return page_rank
 
-nodes_list = np.array(__builtins__.list(graph.nodes()))
-most_interesting = nodes_list[idx_most]
-print(f"most interesting node: {most_interesting}")
+# nodes_list = np.array(__builtins__.list(graph.nodes()))
+# most_interesting = nodes_list[idx_most]
+# print(f"most interesting node: {most_interesting}")
