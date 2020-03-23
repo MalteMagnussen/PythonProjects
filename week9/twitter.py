f1f6dc102abee99231556e80ad2e59c2a8279d17
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-23 11:32:03 +0100

    i have no fucking idea what i just spent 3 hours on

--- week9/twitter.py
+++ week9/twitter.py
@@ -1,14 +1,33 @@
-import wget
-
-!wget https://snap.stanford.edu/data/twitter_combined.txt.gz
-
-!gunzip twitter_combined.txt.gz
-
+import webget
 import networkx as nx
+import gzip
+import numpy as np
 
-g = nx.read_edgelist('twitter_combined.txt')
+# webget.download("https://snap.stanford.edu/data/twitter_combined.txt.gz")
 
-print(nx.info(g))
+with gzip.open("twitter_combined.txt.gz", "r") as f:
+    graph = nx.read_edgelist(f)
 
-print('214328887' in g)
 
+print(nx.info(graph))
+
+print("214328887" in graph)
+
+# in_deg_vec = np.array([graph.in_degree(n) for n in graph.nodes()])
+# max_ind_deg = in_deg_vec.max()
+
+# print(np.argmax(in_deg_vec))
+# print(graph.node[np.argmax(in_deg_vec)]["name"])
+
+# d = 0.85
+# n = graph.number_of_nodes()
+# sub_term = (1 - d) / n
+
+# def page_rank(p):
+
+#     page_rank = sub_term  + (d * pass)
+#     return page_rank
+
+nodes_list = np.array(__builtins__.list(graph.nodes()))
+most_interesting = nodes_list[idx_most]
+print(f"most interesting node: {most_interesting}")
