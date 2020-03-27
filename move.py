0bf2affb1e8822f03a6970c51e26b39dcef43931
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-27 12:08:34 +0100

    cool script i made for my startup

---
+++ move.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+root_src_dir = './satellite-energy-solutions/build'
+root_dst_dir = './trade-wind.github.io'
+
+for src_dir, dirs, files in os.walk(root_src_dir):
+    dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    for file_ in files:
+        src_file = os.path.join(src_dir, file_)
+        dst_file = os.path.join(dst_dir, file_)
+        if os.path.exists(dst_file):
+            # in case of the src and dst are the same file
+            if os.path.samefile(src_file, dst_file):
+                continue
+            os.remove(dst_file)
+        shutil.move(src_file, dst_dir)
