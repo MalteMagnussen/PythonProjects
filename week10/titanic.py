180721becf32b5f7bf264ca4ce93a7140cad13bb
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-30 18:21:54 +0200

    Update titanic.py

--- week10/titanic.py
+++ week10/titanic.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+
 # Task: Get the data on people on the Titanic, their class, sex age, ticket price and whether they survived.
 # Answer: I manually downloaded the .zip file and extracted the files.
 
@@ -32,7 +35,24 @@ print(titanic_data.head())
 
 
 # Task: One-hot-encode the Embarked column (3 different ports in England)
+# Answer:
+# One-hot encoding of 'Embarked' with pd.get_dummies
+titanic_data = pd.get_dummies(titanic_data, columns=["Embarked"])
+print()
+print("One hot encoding")
+print(titanic_data.head())
+
+
 # Task: Drop rows with missing values
+# Find missing values in the data and drop those rows:
+print()
+print("rows before drop n/a", len(titanic_data))
+missing = titanic_data[titanic_data.isnull().any(axis=1)]
+titanic_data = titanic_data.dropna()
+print("rows after", len(titanic_data))
+titanic_data
+
+
 # Task: what is the best bandwidth to use for our dataset? Use sklearn
 # Task: Fit data to a meanshift model
 # Task: How many clusters do we get
