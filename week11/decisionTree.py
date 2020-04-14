97f4022ec8e8b9a13e0bfb9965bc0c090d2e2f5c
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-14 14:25:11 +0200

    Update decisionTree.py

--- week11/decisionTree.py
+++ week11/decisionTree.py
@@ -6,6 +6,15 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
+names = ["Target", "Balance", "Scale", "Weight", "Distance"]
+
 with open("balance-scale.data", "r") as f:
-    df = pd.read_csv(f)
+
+    df = pd.read_csv(f, header=None, names=names)
     print(df.head())
+    # Splitting the data
+    X = df.values[:, 1:5]
+    Y = df.values[:, 0]
+    # Why? What are we doing here?
+    print("\nX\n", X)
+    print("\nY\n", Y)
