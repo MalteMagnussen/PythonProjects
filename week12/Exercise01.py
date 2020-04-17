70dac5017980c8f30294f2cbd98e5bfd905bfaa7
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-17 18:26:23 +0200

    Update Exercise01.py

--- week12/Exercise01.py
+++ week12/Exercise01.py
@@ -89,6 +89,44 @@ with open("rodents.csv", "r") as f:
         print(perceptron(rodent, sixWeights))
 
     # Find the (approximately) optimal weights using the perceptron learning algorithm
+
+    def pla(training_data, no_iterations=10000, eta=0.5):
+        """
+        Find the proper weights to use in the perceptron based on data and target
+        Parameters:
+        training_data: list of vectors, as predictors zipped with a target value
+        no_iterations: number of times to adjust the weights to get them as close as possible to the optimal number
+        eta: the learning rate (prevent learning to go pendulum from one extreme error to the opposite)
+        """
+
+        dim = len(training_data[0][0])
+        weights = np.random.random(
+            dim
+        )  # error and weights (for x and y) start out as random numbers
+
+        # initial_error
+        error = np.random.random()
+        weight_history = [np.copy(weights)]
+
+        for i in range(no_iterations):
+
+            # breakpoint()
+            inp_vec, expected_label = training_data[
+                i % len(training_data)
+            ]  # expected labels are 1 or -1
+            perceptron_output = perceptron(
+                inp_vec, weights
+            )  # perceptron output id a decimal between 0 and 1
+            error = expected_label - perceptron_output  # error
+            weights += eta * error * inp_vec  # accumulate the weights
+            weight_history.append(np.copy(weights))
+
+        return weights, weight_history
+
+    learned_weights, weight_history = pla(df)
+    # print(weight_history)
+    print("LEARNED WEIGHTS", learned_weights)
+
     # Plot the weightline
     # Plot the division line
 
