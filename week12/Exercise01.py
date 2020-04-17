db03c6fa378464b79dcff534579633e58cde2710
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-17 15:42:22 +0200

    Update Exercise01.py

--- week12/Exercise01.py
+++ week12/Exercise01.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import matplotlib
+import matplotlib.pyplot as plt
 
 # weight;height;type
 # 233.4799204165095;23.514129929623852;rat
@@ -17,10 +19,30 @@ from sklearn.metrics import accuracy_score
 
 with open("rodents.csv", "r") as f:
 
-    df = pd.read_csv(f)
+    df = pd.read_csv(f, sep=";")
     print(df.head())
     # Splitting the data
     weightHeight = df.values[:, 0:2]
     rodentType = df.values[:, 2]
-    print("\nWeight and Height\n", weightHeight.head())
-    print("\ntype of rodent\n", rodentType.head())
+    # print("\nWeight and Height\n", weightHeight)
+    # print("\ntype of rodent\n", rodentType)
+
+
+def activation_function(x):
+    """
+    Step function to respond with y = 1 or -1
+    Parameter:
+    x: An x (numeric) value that will have a corresponding y value of 1 or -1
+    """
+    if x < 0:
+        return -1
+    else:
+        return 1
+
+
+rnge = np.linspace(-10.0, 10.0, num=1000)
+print(rnge[0:5])
+values = [activation_function(i) for i in rnge]
+plt.plot(rnge, values)
+plt.axis([-10, 9, -2, 2])
+plt.show()
